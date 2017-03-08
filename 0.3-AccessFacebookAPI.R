18de65c2268bda371664c7d0638b1d5a071b0cb9
Warner <rampy@vt.edu>
2017-03-08 13:41:12 -0500

    ExtractPostData() function added

--- 0.3-AccessFacebookAPI.R
+++ 0.3-AccessFacebookAPI.R
@@ -2,6 +2,23 @@ library(Rfacebook)
 library(dplyr)
 library(jsonlite)
 source("creds.R")
+ExtractPostData <- function(postList){
+  pd <- lapply(postList, "[[", 1) 
+  pd <- unlist(pd)
+  
+  postDataNames <- unique(names(pd))
+  
+  postDF <- data.frame(matrix(ncol = length(postDataNames),
+                              nrow = length(postList)))
+  
+  names(postDF) <- postDataNames
+  
+  for(x in 1:ncol(postDF)){
+    postDF[x] <- pd[names(pd)==names(postDF)[x]]
+    
+  }
+  postDF
+}
 
 token <- fbOAuth(app_id, app_secret)
 
@@ -13,7 +30,7 @@ trumpsPage <- getPage("DonaldTrump", token, n = 2000)
 nytPage <- getPage("nytimes", token , n = 2000)
 
 
-
+# this should be functionized with startingIndex arg
 trumpsPosts <- vector("list", 5)
 # 5 trump posts took 7 minutes, 32.3 mb list
 for(x in 1:length(trumpsPosts)){
@@ -22,8 +39,19 @@ for(x in 1:length(trumpsPosts)){
                               n.comments = trumpsPage$comments_count[x])
 }
 
-a1 <- lapply(trumpsPosts, "[[", 1) 
-# length(unlist(a1)) is an option
+
+
+
+
+
+
+
+
+
+
+
+
+
 a1 %>%
   Reduce( function(df1, df2) full_join(df1, df1), .) ->
 a2
