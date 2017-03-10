48356e5e8ac17c9170249156c9a55b13517274ee
Warner <rampy@vt.edu>
2017-03-10 00:21:06 -0500

    initial commit

--- facebook/deploy_sqlite.R
+++ facebook/deploy_sqlite.R
@@ -1,5 +1,6 @@
 library(RSQLite)
 library(sqldf)
+library(lubridate)
 source("creds.R")
 source("FacebookFunctions.R")
 
@@ -8,10 +9,15 @@ fb_to_db <- function(dbConnection, fbPageName, authToken, dbTableName){
   message("Fetching data")
   
   page <- getPage(fbPageName, token, n = 2000)
+  page$created_time <- ymd_hms(page$created_time)
+  page$day_created <- day(page$created_time)
+  page$dow_created <- wday(page$created_time)
+  page$month_created <- month(page$created_time)
+  page$created_time <- gsub(" UTC", "", page$created_time)
   
   message("Disconnect from db before writing ...")
   readline("Press enter to write table to db:")
-  
+
   dbWriteTable(conn = dbConnection, name = dbTableName, 
                value = page, overwrite = TRUE)
 }
@@ -22,7 +28,14 @@ token <- fbOAuth(app_id, app_secret)
 # set up connection to sqlite db
 db <- dbConnect(RSQLite::SQLite(), dbname="omtm.sqlite")
 
-# write fb data into db
+# ----------------------------------------------------------
+#
+# writing various tables to sqlite database
+#
+#--------------------------------------------------------------
+
+# write fb data into db 
+# ----------------------------------------------------------
 fb_to_db(db, "DonaldTrump", token, "trump")
 fb_to_db(db, "Breitbart", token, "breitbart")
 fb_to_db(db, "FoxNews", token, "fox")
@@ -34,6 +47,7 @@ fb_to_db(db, "bbcnews", token, "bbc")
 fb_to_db(db, "TheEconomist", token, "economist")
 
 # extract data from posts and write them into new db tables
+# ----------------------------------------------------------
 ExtractPostData(trumpsPosts) %>%
   dbWriteTable(conn = db, name = "posts", overwrite = TRUE)
 
