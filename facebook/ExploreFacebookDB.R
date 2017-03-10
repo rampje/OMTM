ed804ac074fab5899dcf6f94351a3c6bef6f4b32
Warner <rampy@vt.edu>
2017-03-09 22:24:18 -0500

    bigger scope db

--- facebook/ExploreFacebookDB.R
+++ facebook/ExploreFacebookDB.R
@@ -11,3 +11,8 @@ comments <- dbGetQuery(db, "SELECT * FROM postcomments")
 plot(posts$likes_count, posts$comments_count)
 # the video and photo with 100000 + likes are special
 # the other 8 look like normy posts
+
+
+dbSendQuery(db,"INSERT INTO posts values(1,2,3,4,5,6,7,8,9,10)")
+
+dbGetQuery(db, "SELECT * FROM posts")
---
+++ facebook/deploy_sqlite.R
@@ -0,0 +1,46 @@
+library(RSQLite)
+library(sqldf)
+source("creds.R")
+source("FacebookFunctions.R")
+
+# download facebook page data and write to db
+fb_to_db <- function(dbConnection, fbPageName, authToken, dbTableName){
+  message("Fetching data")
+  
+  page <- getPage(fbPageName, token, n = 2000)
+  
+  message("Disconnect from db before writing ...")
+  readline("Press enter to write table to db:")
+  
+  dbWriteTable(conn = dbConnection, name = dbTableName, 
+               value = page, overwrite = TRUE)
+}
+
+# set up authentication token
+token <- fbOAuth(app_id, app_secret)
+
+# set up connection to sqlite db
+db <- dbConnect(RSQLite::SQLite(), dbname="omtm.sqlite")
+
+# write fb data into db
+fb_to_db(db, "DonaldTrump", token, "trump")
+fb_to_db(db, "Breitbart", token, "breitbart")
+fb_to_db(db, "FoxNews", token, "fox")
+fb_to_db(db, "foxandfriends", token, "faf")
+fb_to_db(db, "nytimes", token, "nyt")
+fb_to_db(db, "washingtonpost", token, "wp")
+fb_to_db(db, "cnn", token, "cnn")
+fb_to_db(db, "bbcnews", token, "bbc")
+fb_to_db(db, "TheEconomist", token, "economist")
+
+# extract data from posts and write them into new db tables
+ExtractPostData(trumpsPosts) %>%
+  dbWriteTable(conn = db, name = "posts", overwrite = TRUE)
+
+ExtractPostData(trumpsPosts, type="likes") %>%
+  dbWriteTable(conn = db, name = "postlikes", overwrite = TRUE)
+
+ExtractPostData(trumpsPosts, type="comments") %>%
+  dbWriteTable(conn = db, name = "postcomments", overwrite = TRUE)
+
+dbDisconnect(db)
