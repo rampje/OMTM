be324aa06f89c321cc927ebf462a5bd6c8dd55ff
Warner <rampy@vt.edu>
2017-04-02 21:40:50 -0400

    Developing news_crawler

---
+++ News Desaturator/news_crawler.py
@@ -0,0 +1,29 @@
+# add comment for fun
+
+# initial commit using example from documentation
+# run "scrapy runspider crawler1.py -o quotes.json in" terminal
+
+
+import scrapy
+
+class NewsArticle(scrapy.Item):
+    title = scrapy.Field()
+    text = scrapy.Field()
+    link = scrapy.Field()
+
+class NewsSpider(scrapy.Spider):
+    name = 'newsspider'
+    start_urls = ['http://www.politico.com',
+                  'http://www.nytimes.com']
+
+    def parse(self, response):
+        for url in response.xpath('//a'):
+            news = NewsArticle()
+
+            news['title'] = "blah"
+            news['text'] = "blah2"
+            news['link'] = url.extract()
+
+            yield news
+
+
---
+++ creds.R
@@ -0,0 +1,8 @@
+app_id <- 593045154223411
+app_secret <- "7fe845d0e32aebd43a107d14856be66c"
+news.API.key <- "3ea7ac10835f464bafa4852873b21408"
+# Set twitter API Keys
+api_key <- "ILZof5KEeOJqGsmBOZ3sWqlk4"
+api_secret <- "CeydSs6AIzQEAH7w6DnVnqAmdYRLaWmV9yo47uxAxGlNPeYy8x"
+access_token <- "838147378922340352-cu9GDw4XlNp7LVYYPyFOdMqPNq9KLbn"
+access_token_secret <- "NwcfOivECg44mRYLjc5hLQCN6ooANaMVxziKP7d4VnrS6"
